c4a87491020ab98c865e5c1fb6ab59a581597fa7
prasangeet <b23ch1033@iitj.ac.in>
2025-07-12 09:13:22 +0530

    First commit

---
+++ backend/models/userModel.js
@@ -0,0 +1,16 @@
+import pool from "../configs/db";
+
+export const createUser = async (name, email, passwordHash) => {
+  const result = await pool.query(
+    `INSERT INTO users (name, email, password_hash) VALUES ($1, $2, $3) RETURNING *`,
+    [name, email, passwordHash]
+  );
+  return result.rows[0];
+};
+
+export const findUserByEmail = async (email) => {
+  const result = await pool.query(`SELECT * FROM users WHERE email = $1`, [
+    email,
+  ]);
+  return result.rows[0];
+};
